9e1e6c8f74fd6f2d4dd38af0a661473ebf6445de
mpapila <mehmetpapila95@gmail.com>
2024-03-23 14:09:34 -0500

    Added HTML implementation using DOM

--- main.js
+++ main.js
@@ -2,27 +2,32 @@ const tictactoe = (function () {
     let PlayerOne = 'X';
     let PlayerTwo = 'O'
     let board = ['', '', '', '', '', '', '', '', ''];
-    let getBoard = function () {
-        return board;
-    };
-    let resetBoard = ['', '', '', '', '', '', '', '', ''] // use this one to reset the game after finishes
-
+    function resetBoard() {
+        board = ['', '', '', '', '', '', '', '', ''] // use this one to reset the game after finishes
+        currentPlayer = 1;
+        document.querySelectorAll('.btn').forEach(button => {
+            button.textContent = '';
+        });
+    }
     let currentPlayer = 1;
     let markSpot = function (position) {
         if (currentPlayer === 1) {
             if (position === '' || (position >= 0 && position <= 9)) {
                 board.splice(position, 1, PlayerOne);
-                if (board[0] && board[1] && board[2] === PlayerOne ||
-                    board[3] && board[4] && board[5] === PlayerOne ||
-                    board[6] && board[7] && board[8] === PlayerOne ||
-                    board[0] && board[3] && board[6] === PlayerOne ||
-                    board[1] && board[4] && board[7] === PlayerOne ||
-                    board[2] && board[5] && board[8] === PlayerOne ||
-                    board[0] && board[4] && board[8] === PlayerOne ||
-                    board[2] && board[4] && board[6] === PlayerOne
+                // document.querySelector('.cell').style.display = "none";
+                document.querySelectorAll('.cell')[position].textContent = PlayerOne
+                if (board[0] === PlayerOne && board[1] === PlayerOne && board[2] === PlayerOne  ||
+                    board[3] === PlayerOne && board[4] === PlayerOne && board[5] === PlayerOne  ||
+                    board[6] === PlayerOne && board[7] === PlayerOne && board[8] === PlayerOne  ||
+                    board[0] === PlayerOne && board[3] === PlayerOne && board[6] === PlayerOne  ||
+                    board[1] === PlayerOne && board[4] === PlayerOne && board[7] === PlayerOne  ||
+                    board[2] === PlayerOne && board[5] === PlayerOne && board[8] === PlayerOne  ||
+                    board[0] === PlayerOne && board[4] === PlayerOne && board[8] === PlayerOne  ||
+                    board[2] === PlayerOne && board[4] === PlayerOne && board[6] === PlayerOne 
                 ) {
                     console.log(board)
                     alert('Player One won')
+                    resetBoard();
                 } else {
                     console.log(board)
                     currentPlayer = 2;
@@ -32,17 +37,19 @@ const tictactoe = (function () {
             }
         } else {
             board.splice(position, 1, PlayerTwo);
-            if (board[0] && board[1] && board[2] === PlayerTwo ||
-                board[3] && board[4] && board[5] === PlayerTwo ||
-                board[6] && board[7] && board[8] === PlayerTwo ||
-                board[0] && board[3] && board[6] === PlayerTwo ||
-                board[1] && board[4] && board[7] === PlayerTwo ||
-                board[2] && board[5] && board[8] === PlayerTwo ||
-                board[0] && board[4] && board[8] === PlayerTwo ||
-                board[2] && board[4] && board[6] === PlayerTwo
+            document.querySelectorAll('.cell')[position].textContent = PlayerTwo
+            if (board[0] === PlayerTwo && board[1] === PlayerTwo && board[2] === PlayerTwo  ||
+                board[3] === PlayerTwo && board[4] === PlayerTwo && board[5] === PlayerTwo  ||
+                board[6] === PlayerTwo && board[7] === PlayerTwo && board[8] === PlayerTwo  ||
+                board[0] === PlayerTwo && board[3] === PlayerTwo && board[6] === PlayerTwo  ||
+                board[1] === PlayerTwo && board[4] === PlayerTwo && board[7] === PlayerTwo  ||
+                board[2] === PlayerTwo && board[5] === PlayerTwo && board[8] === PlayerTwo  ||
+                board[0] === PlayerTwo && board[4] === PlayerTwo && board[8] === PlayerTwo  ||
+                board[2] === PlayerTwo && board[4] === PlayerTwo && board[6] === PlayerTwo 
             ) {
                 console.log(board)
                 alert('Player Two won')
+                resetBoard();
             } else {
                 console.log(board)
                 currentPlayer = 1
@@ -52,24 +59,20 @@ const tictactoe = (function () {
     };
 
     return {
-        getBoard,
-        markSpot
+        markSpot,
+        
     };
 })();
 
+var buttons = document.querySelectorAll('.btn');
+buttons.forEach(function(button, index) {
+    button.addEventListener('click', function() {
+        tictactoe.markSpot(index);
+    });
+});
 
 
-// let currentPlayer = 1;
-// var buttons = document.querySelectorAll('.btn');
-// buttons.forEach(function(button) {
-//     button.addEventListener('click', function() {
-//       if (currentPlayer === 1) {
-//         console.log('Player 1 clicked a button');
-//         currentPlayer = 2;
-//       } else {
-//         console.log('Player 2 clicked a button');
-//         currentPlayer = 1;
-//       }
-//     });
-//   });
+
+
+
 
